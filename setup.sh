165c5f885b11a503f5daa0b127b8148e32dfc687
l1s22bsse0184 <l1s22bsse0184@ucp.edu.pk>
2025-07-04 17:10:01 +0500

    ïnitial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init
+git branch -m develop 
+mkdir ./src ./styles ./asserts ./bin  ./node_modules 
+touch ./.gitignore ./src/index.html ./styles/index.css
+
+git config --local user.name "l1s22bsse0184"
+git config --local user.email "l1s22bsse0184@ucp.edu.pk"
+git config --local core.editor "notepad"
+git remote add origin https://github.com/Khawaja-0184/project_s25_s2.git
